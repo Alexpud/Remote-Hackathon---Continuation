5f73b74b6d5a0aecf7ccf66a0d7514f7ffa13792
Alexandre <alexandre.maciele@gmail.com>
2020-03-25 23:09:07 -0300

    First commit

---
+++ infrastructure/Repositories/HackathonRepository.cs
@@ -0,0 +1,13 @@
+using Domain.Entities;
+using Domain.Repositories;
+
+namespace Infrastructure.Repositories
+{
+    public class HackathonRepository : IHackathonRepository
+    {
+        public Hackathon CreateHackathon(Hackathon hackathon)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
