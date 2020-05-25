d343c02bfb7e47ffa26d4cd536791b4b51c6068e
Alexandre <alexandre.maciele@gmail.com>
2020-05-25 00:27:16 -0300

    Modifications to the Infrastructure

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Domain.Repositories;
 using Domain.Services.Concrete;
@@ -9,12 +5,9 @@ using Domain.Services.Interfaces;
 using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Application
 {
